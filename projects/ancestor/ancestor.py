27c2c21ee682dc6441499565c2ea54a1ef1d4981
jonathandly <dalyj138@gmail.com>
2020-04-27 17:18:45 -0400

    Partial completion of earliest_ancestor function. Currently unsure of what needs to be changed. Need to review previous lecture to figure this out.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    vertices = {}
+    if starting_node not in vertices:
+        return -1
+
+    arr = []
+    arr.append(starting_node)
+    visited = set()
+    
+    while len(arr) > 0:
+        vertex = arr.pop()
+        if len(arr) == 1:
+            return vertex
+
+        if vertex not in visited:
+            visited.add(vertex)
+
+            for vert in vertices[vertex]:
+                arr.append(vert)
